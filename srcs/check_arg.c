e13c74320c16173fbdbe0bd2f1b62add5da56df4
kiri0314 <t.kirihara0314@gmail.com>
2021-12-15 00:20:55 +0900

    [add]check_argの追加

--- srcs/check_arg.c
+++ srcs/check_arg.c
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   check_arg.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/14 18:07:23 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/14 18:45:36 by tkirihar         ###   ########.fr       */
+/*   Created: 2021/12/14 18:49:00 by tkirihar          #+#    #+#             */
+/*   Updated: 2021/12/14 22:13:19 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(int ac, char **av)
+static void	check_extension(char *map_file)
 {
-	check_arg(ac, av);
-	return (0);
+	char	*extension;
+
+	extension = ft_strrchr(map_file, '.');
+	if (!extension || ft_strncmp(extension, ".ber", 4))
+		exit_and_put_error("the map file could not be loaded.");
+}
+
+void	check_arg(int ac, char **av)
+{
+	if (ac != 2)
+		exit_and_put_error("the number of arguments is different");
+	check_extension(av[1]);
 }
