62e679298e92fee4519490e3e447e296da206c85
kiri0314 <t.kirihara0314@gmail.com>
2021-12-21 02:51:16 +0900

    [add]make_map関数を追加

--- srcs/read_map_file.c
+++ srcs/read_map_file.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 18:23:36 by tkirihar          #+#    #+#             */
-/*   Updated: 2021/12/21 01:59:13 by tkirihar         ###   ########.fr       */
+/*   Updated: 2021/12/21 02:50:34 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,36 @@ static void	count_line(t_data *game_data, char *map_file)
 	close(fd);
 }
 
+static void	make_map(t_data *game_data, char *map_file)
+{
+	char	*line;
+	int		fd;
+	size_t	i;
+
+	game_data->map = (char **)malloc(sizeof(char *) * game_data->map_height);
+	if (game_data->map == NULL)
+		exit(1);
+	fd = open(map_file, O_RDONLY);
+	i = 0;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		game_data->map[i] = line;
+		i++;
+	}
+}
+
+// static void	check_map(t_data *game_data)
+// {
+
+// }
+
 /* map_fileを読み込む関数 */
 void	read_map_file(t_data *game_data, char *map_file)
 {
 	count_line(game_data, map_file);
-	printf("%d", game_data->map_height);
+	make_map(game_data, map_file);
+	// check_map(game_data);
 }
