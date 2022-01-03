9cfd54b7f1bce53216e571aa031aef10950e4c27
kiri0314 <t.kirihara0314@gmail.com>
2022-01-03 15:49:26 +0900

    add:init_mlx.c

--- srcs/init_mlx.c
+++ srcs/init_mlx.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_mlx.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/14 18:07:23 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/03 15:42:02 by tkirihar         ###   ########.fr       */
+/*   Created: 2022/01/03 15:47:01 by tkirihar          #+#    #+#             */
+/*   Updated: 2022/01/03 15:48:00 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	main(int ac, char **av)
+void	init_mlx(t_data *game_data)
 {
-	t_data	game_data;
 
-	check_arg(ac, av);
-	read_map_file(&game_data, av[1]);
-	init_mlx(&game_data);
-	exit(0);
 }
