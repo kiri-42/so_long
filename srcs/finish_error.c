f4f5789f698f93dd61a93c03f31f9e30aea2ddd0
kiri0314 <t.kirihara0314@gmail.com>
2022-01-04 16:28:36 +0900

    change:function comment

--- srcs/finish_error.c
+++ srcs/finish_error.c
@@ -6,13 +6,13 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 00:55:18 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/03 17:02:34 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/04 16:27:40 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-/* エラーメッセージを出力してプログラムを終了させる関数 */
+/* Function to output an error message and exit the program */
 void	finish_error(char *error_message)
 {
 	ft_putstr_fd("\x1b[31mError\n\x1b[39m", STDERR_FILENO);
