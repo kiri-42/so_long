560ec244d6950f80acaae8607447c0b0d53c01b2
kiri0314 <t.kirihara0314@gmail.com>
2022-01-05 21:33:24 +0900

    updata:Load image data

--- srcs/init_mlx.c
+++ srcs/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:47:01 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/05 19:37:40 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/05 21:31:26 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,20 @@
 void	init_mlx(t_data *game_data)
 {
 	game_data->mlx = mlx_init();
-	game_data->win = mlx_new_window(game_data->mlx, game_data->map_width * 20, \
-	game_data->map_height * 20, "so_long");
+	game_data->win = mlx_new_window(game_data->mlx, game_data->map_width * 32, \
+	game_data->map_height * 32, "so_long");
+	game_data->img = mlx_new_image(game_data->mlx, 32, 32);
+	game_data->floor = mlx_xpm_file_to_image(game_data->mlx, FLOOR, \
+	&game_data->width, &game_data->height);
+	game_data->player = mlx_xpm_file_to_image(game_data->mlx, PLAYER, \
+	&game_data->width, &game_data->height);
+	game_data->coin = mlx_xpm_file_to_image(game_data->mlx, COIN, \
+	&game_data->width, &game_data->height);
+	game_data->wall = mlx_xpm_file_to_image(game_data->mlx, WALL, \
+	&game_data->width, &game_data->height);
+	game_data->rock = mlx_xpm_file_to_image(game_data->mlx, ROCK, \
+	&game_data->width, &game_data->height);
+	game_data->exit = mlx_xpm_file_to_image(game_data->mlx, EXIT, \
+	&game_data->width, &game_data->height);
 	mlx_loop(game_data->mlx);
 }
