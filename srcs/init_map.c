30bd06355b5d9db25ecc2e6fa2e7ca5d1981d907
kiri0314 <t.kirihara0314@gmail.com>
2022-01-06 01:32:31 +0900

    upgrade:Add search_object function

--- srcs/init_map.c
+++ srcs/init_map.c
@@ -6,24 +6,44 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 21:50:50 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/05 22:29:25 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/06 01:29:29 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static void	put_object(t_data *game_data, size_t width_i, size_t height_i)
+static void	search_object(t_data *game_data, size_t width_i, size_t height_i)
 {
 	if (game_data->map_data[height_i][width_i] == '1')
-		put_wall(game_data, width_i, height_i);
+	{
+		mlx_put_image_to_window(game_data->mlx, game_data->win, \
+		game_data->wall, game_data->width * width_i, \
+		game_data->height * height_i);
+	}
 	else if (game_data->map_data[height_i][width_i] == '0')
-		put_floor(game_data, width_i, height_i);
+	{
+		mlx_put_image_to_window(game_data->mlx, game_data->win, \
+		game_data->floor, game_data->width * width_i, \
+		game_data->height * height_i);
+	}
 	else if (game_data->map_data[height_i][width_i] == 'P')
-		put_player(game_data, width_i, height_i);
+	{
+		mlx_put_image_to_window(game_data->mlx, game_data->win, \
+		game_data->player, game_data->width * width_i, \
+		game_data->height * height_i);
+	}
 	else if (game_data->map_data[height_i][width_i] == 'C')
-		put_coin(game_data, width_i, height_i);
+	{
+		mlx_put_image_to_window(game_data->mlx, game_data->win, \
+		game_data->coin, game_data->width * width_i, \
+		game_data->height * height_i);
+	}
 	else if (game_data->map_data[height_i][width_i] == 'E')
-		put_exit(game_data, width_i, height_i);
+	{
+		mlx_put_image_to_window(game_data->mlx, game_data->win, \
+		game_data->exit, game_data->width * width_i, \
+		game_data->height * height_i);
+	}
 	else
 		finish_error("the character is not registered as an object.");
 }
@@ -34,15 +54,15 @@ void	init_map(t_data *game_data)
 	size_t	width_i;
 
 	height_i = 0;
-	while (height_i < game_data->map_height)
+	while (height_i < (size_t)game_data->map_height)
 	{
 		width_i = 0;
-		while (width_i < game_data->map_width)
+		while (width_i < (size_t)game_data->map_width)
 		{
-			put_object(game_data, width_i, height_i);
+			search_object(game_data, width_i, height_i);
 			width_i++;
 		}
 		height_i++;
 	}
-	check_map(game_data);
+	// check_map(game_data);
 }
