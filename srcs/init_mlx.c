22cd11750e4d6d0280a0e94e583e6512a6f57665
kiri0314 <t.kirihara0314@gmail.com>
2022-01-05 20:26:33 +0900

    clean

--- srcs/init_mlx.c
+++ srcs/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:47:01 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/04 04:44:23 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/05 19:37:40 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	init_mlx(t_data *game_data)
 {
 	game_data->mlx = mlx_init();
-	game_data->win = mlx_new_window(game_data->mlx, game_data->map_width, \
-	game_data->map_height, "so_long");
+	game_data->win = mlx_new_window(game_data->mlx, game_data->map_width * 20, \
+	game_data->map_height * 20, "so_long");
 	mlx_loop(game_data->mlx);
 }
