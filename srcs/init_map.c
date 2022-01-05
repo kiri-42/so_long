027922f86edc5ef748fdc326565eb40f11de742e
kiri0314 <t.kirihara0314@gmail.com>
2022-01-06 02:00:17 +0900

    upgrade:Add init_cnt, putobject functions

--- srcs/init_map.c
+++ srcs/init_map.c
@@ -6,43 +6,45 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 21:50:50 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/06 01:29:29 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/06 01:55:17 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static void	search_object(t_data *game_data, size_t width_i, size_t height_i)
+static void	init_cnt(t_data *game_data)
 {
-	if (game_data->map_data[height_i][width_i] == '1')
+	game_data->cnt_P = 0;
+	game_data->cnt_C = 0;
+	game_data->cnt_E = 0;
+}
+
+static void	put_object(t_data *game_data, size_t x, size_t y, void *object)
+{
+	mlx_put_image_to_window(game_data->mlx, game_data->win, \
+	object, game_data->width * x, game_data->height * y);
+}
+
+static void	search_object(t_data *game_data, size_t x, size_t y)
+{
+	if (game_data->map_data[y][x] == '1')
+		put_object(game_data, x, y, game_data->wall);
+	else if (game_data->map_data[y][x] == '0')
+		put_object(game_data, x, y, game_data->floor);
+	else if (game_data->map_data[y][x] == 'P')
 	{
-		mlx_put_image_to_window(game_data->mlx, game_data->win, \
-		game_data->wall, game_data->width * width_i, \
-		game_data->height * height_i);
+		put_object(game_data, x, y, game_data->player);
+		game_data->cnt_P++;
 	}
-	else if (game_data->map_data[height_i][width_i] == '0')
+	else if (game_data->map_data[y][x] == 'C')
 	{
-		mlx_put_image_to_window(game_data->mlx, game_data->win, \
-		game_data->floor, game_data->width * width_i, \
-		game_data->height * height_i);
+		put_object(game_data, x, y, game_data->coin);
+		game_data->cnt_C++;
 	}
-	else if (game_data->map_data[height_i][width_i] == 'P')
+	else if (game_data->map_data[y][x] == 'E')
 	{
-		mlx_put_image_to_window(game_data->mlx, game_data->win, \
-		game_data->player, game_data->width * width_i, \
-		game_data->height * height_i);
-	}
-	else if (game_data->map_data[height_i][width_i] == 'C')
-	{
-		mlx_put_image_to_window(game_data->mlx, game_data->win, \
-		game_data->coin, game_data->width * width_i, \
-		game_data->height * height_i);
-	}
-	else if (game_data->map_data[height_i][width_i] == 'E')
-	{
-		mlx_put_image_to_window(game_data->mlx, game_data->win, \
-		game_data->exit, game_data->width * width_i, \
-		game_data->height * height_i);
+		put_object(game_data, x, y, game_data->exit);
+		game_data->cnt_E++;
 	}
 	else
 		finish_error("the character is not registered as an object.");
@@ -53,6 +55,7 @@ void	init_map(t_data *game_data)
 	size_t	height_i;
 	size_t	width_i;
 
+	init_cnt(game_data);
 	height_i = 0;
 	while (height_i < (size_t)game_data->map_height)
 	{
