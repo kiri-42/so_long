b24c3444c00f572089ef7fc6c0264263520fa403
kiri0314 <t.kirihara0314@gmail.com>
2022-01-06 10:34:07 +0900

    update:Replacing the execution order

--- srcs/check_map_data.c
+++ srcs/check_map_data.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 01:13:06 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/05 17:27:38 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/06 10:30:54 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,23 @@ static void	is_3_or_more_in_length(t_data *game_data)
 	{
 		// free_all();
 		finish_error("map height or width is too small");
+	}
+}
+
+/* Function to check if the width of map is equal */
+static void	is_equal_in_width(t_data *game_data)
+{
+	size_t	row_i;
+
+	row_i = 0;
+	while (row_i < (size_t)game_data->map_height)
+	{
+		if (ft_strlen(game_data->map_data[row_i]) != (size_t)game_data->map_width)
+		{
+			// free_all();
+			finish_error("the map is not rectangular");
+		}
+		row_i++;
 	}
 }
 
@@ -52,27 +69,10 @@ static void	is_surrounded_by_wall(t_data *game_data)
 	}
 }
 
-/* Function to check if the width of map is equal */
-static void	is_equal_in_width(t_data *game_data)
-{
-	size_t	row_i;
-
-	row_i = 0;
-	while (row_i < (size_t)game_data->map_height)
-	{
-		if (ft_strlen(game_data->map_data[row_i]) != (size_t)game_data->map_width)
-		{
-			// free_all();
-			finish_error("the map is not rectangular");
-		}
-		row_i++;
-	}
-}
-
 /* Function to check if the map data is in the correct format */
 void	check_map_data(t_data *game_data)
 {
 	is_3_or_more_in_length(game_data);
-	is_surrounded_by_wall(game_data);
 	is_equal_in_width(game_data);
+	is_surrounded_by_wall(game_data);
 }
