f3247d79a903cda7d12a34ff9529efd57ff9f2b9
kiri0314 <t.kirihara0314@gmail.com>
2022-01-07 18:28:50 +0900

    upgrade:add function comment

--- srcs/close_game.c
+++ srcs/close_game.c
@@ -6,12 +6,13 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 18:19:54 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/07 18:21:04 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/07 18:27:18 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+/* Function to close the window */
 int	close_game(t_data *game_data)
 {
 	mlx_destroy_window(game_data->mlx, game_data->win);
